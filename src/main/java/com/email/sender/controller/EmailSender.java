9111b1b200318483422bc46c41b00b25581c40ee
Jesus Enrique Aldana Sanchez <jealdana@gmail.com>
2025-06-10 17:01:07 -0600

    commit inicial

---
+++ src/main/java/com/email/sender/controller/EmailSender.java
@@ -0,0 +1,56 @@
+package com.email.sender.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.email.sender.entity.EmailMessage;
+import com.email.sender.service.EmailService;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+
+@RestController
+public class EmailSender {
+
+	@Autowired
+	EmailService emailService;
+	
+	
+
+	
+	@PostMapping("/email/send")
+	public String emailSend(
+			@RequestBody EmailMessage emailMessage) {	
+		
+		String response = "Correo enviado...";
+		 try {
+			emailService.sendHtmlEmail(emailMessage.getFrom(), emailMessage.getTo(), emailMessage.getSubject(), emailMessage.getBody());
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			response = "Ha habido un error al enviar el correo.";
+		}
+		
+		 return response;
+	}
+	
+	@GetMapping("/email/test")
+	public String getEmailSend() {
+		try {
+			emailService.sendHtmlEmail("jealdana@cerometros.com", "jealdana@gmail.com", "Prueba de correo html", 
+					"<h1>Esta es una prueba de email desde Spring</h1>" + "<p>Puede contener <strong>Contenido</strong> HTML.</p>");
+		
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "Correo enviado";
+				//emailService.sendEmail(emailMessage.getTo(), emailMessage.getSubject(), emailMessage.getBody());
+	}
+}
